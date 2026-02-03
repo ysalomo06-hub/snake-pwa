cab0545e1754fc1d8fa3a279a4a4b60d9fb4f7e4
ysalomo06-hub <y.salomo06@gmail.com>
2026-02-03 18:10:32 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "snake-pwa-v1";
+const CACHE_NAME = "snake-pwa-v2"; // NAIKKAN VERSI
 const FILES = [
   "index.html",
   "style.css",
@@ -9,6 +9,17 @@ const FILES = [
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
+    )
   );
 });
 
